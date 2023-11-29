4d28e2dbd6dfad892c6f6d4d243d2f5ed0064ed0
kawaken666 <kawaken666.2@gmail.com>
2023-11-30 00:55:12 +0900

    表の見た目を整形した

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,22 +9,24 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    <tr>
-                        <th>プロジェクト名</th>
-                        <th>見積もり</th>
-                        <th>リリース予定日</th>
-                        <th>稼働予定月</th>
-                    </tr>
-                    <br>
-                    @foreach($projects as $project)
+                    <table class="table-auto text-center border-2">
                         <tr>
-                            <td>{{ $project->name }}</td>
-                            <td>{{ $project->estimation }}</td>
-                            <td>{{ $project->release_date }}</td>
-                            <td>{{ $project->work_date }}</td>
+                            <th class="border-2">プロジェクト名</th>
+                            <th class="border-2">見積もり</th>
+                            <th class="border-2">リリース予定日</th>
+                            <th class="border-2">稼働予定月</th>
                         </tr>
                         <br>
-                    @endforeach
+                        @foreach($projects as $project)
+                            <tr>
+                                <td class="border-2">{{ $project->name }}</td>
+                                <td class="border-2">{{ $project->estimation }}</td>
+                                <td class="border-2">{{ $project->release_date }}</td>
+                                <td class="border-2">{{ $project->work_date }}</td>
+                            </tr>
+                            <br>
+                        @endforeach
+                    </table>
                 </div>
             </div>
         </div>
